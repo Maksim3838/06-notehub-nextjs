ac5d980fc9602cf63764eb6dbdb72398479adf42
Maksim <70702008m@gmail.com>
2025-12-07 13:20:44 +0200

    text

--- lib/api.ts
+++ lib/api.ts
@@ -20,8 +20,7 @@ const myToken = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 axios.defaults.baseURL = 'https://next-v1-notes-api.goit.study';
 
 export const getNotes = async (cstegoryId?: string) => {
-  // const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-  // await delay(2000);
+  
   const res = await axios.get('/notes', {
     params: { cstegoryId },
     headers: {
@@ -36,28 +35,3 @@ export async function getSingleNote(id: string) {
   return response.data;
 }
 
-// lib/api.ts
-
-// import axios from 'axios';
-
-// export type Note = {
-//   id: string;
-//   title: string;
-//   content: string;
-//   categoryId: string;
-//   userId: string;
-//   createdAt: string;
-//   updatedAt: string;
-// };
-
-// export type NoteListResponse = {
-//   notes: Note[];
-//   total: number;
-// };
-
-// axios.defaults.baseURL = 'https://next-v1-notes-api.goit.study';
-
-// export const getNotes = async () => {
-//   const res = await axios.get<NoteListResponse>('/notes');
-//   return res.data;
-// };
