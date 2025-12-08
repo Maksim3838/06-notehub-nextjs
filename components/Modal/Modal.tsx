ecbe0305a8d404a6b97b83912251de061866fda0
Maksim <70702008m@gmail.com>
2025-12-08 22:30:23 +0200

    text

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,41 +1,50 @@
+'use client';
+
 import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import type { ReactNode } from "react";
-import css from "../Modal/Modal.module.css"
+import css from "./Modal.module.css";
 
 interface ModalProps {
-   onClose: () => void;
+  onClose: () => void;
   children: ReactNode;
 }
 
 export default function Modal({ onClose, children }: ModalProps) {
-    
-    useEffect(() => {
-        const handleEsc = (event: KeyboardEvent) => {
-      if (event.key === "Escape") onClose();
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
     };
 
     document.addEventListener("keydown", handleEsc);
     document.body.style.overflow = "hidden";
 
-        return () => {
+    return () => {
       document.removeEventListener("keydown", handleEsc);
       document.body.style.overflow = "";
     };
   }, [onClose]);
 
-    return createPortal(
-    <div className={css.backdrop}
-      onClick={onClose}
-          >
-      <div className={css.modal}
-        onClick={(e) => e.stopPropagation()}
-              >
+  if (typeof document === "undefined") return null;
+
+  return createPortal(
+    <div
+      className={css.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={(e) => {
+        
+        if (e.target === e.currentTarget) {
+          onClose();
+        }
+      }}
+    >
+      <div className={css.modal}>
         {children}
       </div>
     </div>,
     document.body
   );
 }
-
-
--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import type { Note } from "../../types/note";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteNote } from "../../services/noteService"; 
@@ -20,25 +21,26 @@ export default function NoteList({ notes }: NoteListProps) {
   if (!notes.length) return <p>No notes found</p>;
 
   return (
-
     <ul className={css.list}>
-	{notes.map(note=>(<li key={note.id} className={css.listItem}>
-    <h2 className={css.title}>{note.title}</h2>
-    <p className={css.content}>{note.content}</p>
-    <div className={css.footer}>
-      <span className={css.tag}>{note.tag}</span>
-      <button
-  className={css.button}
-  onClick={() => deleteMutation.mutate(note.id)}
->
-  Delete
-</button>
-
-    </div>
-  </li>))}
-  </ul>
-   )
-  }
-
+      {notes.map(note => (
+        <li key={note.id} className={css.listItem}>
+          <Link href={`/notes/${note.id}`}>
+            <h2 className={css.title}>{note.title}</h2>
+          </Link>
+          <p className={css.content}>{note.content}</p>
+          <div className={css.footer}>
+            <span className={css.tag}>{note.tag}</span>
+            <button
+              className={css.button}
+              onClick={() => deleteMutation.mutate(note.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
   
