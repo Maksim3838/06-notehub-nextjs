fce0c93486e23ea3510b600bba13fa37022642aa
Maksim <70702008m@gmail.com>
2025-12-28 17:02:18 +0200

    text

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,3 +1,59 @@
+// 'use client';
+
+// import { useEffect } from 'react';
+// import { createPortal } from 'react-dom';
+// import type { ReactNode } from 'react';
+// import css from './Modal.module.css';
+
+// interface ModalProps {
+//   isOpen: boolean;
+//   onClose: () => void;
+//   children: ReactNode;
+// }
+
+// export default function Modal({
+//   isOpen,
+//   onClose,
+//   children,
+// }: ModalProps) {
+//   useEffect(() => {
+//     if (!isOpen) return;
+
+//     const handleEsc = (event: KeyboardEvent) => {
+//       if (event.key === 'Escape') {
+//         onClose();
+//       }
+//     };
+
+//     document.addEventListener('keydown', handleEsc);
+//     document.body.style.overflow = 'hidden';
+
+//     return () => {
+//       document.removeEventListener('keydown', handleEsc);
+//       document.body.style.overflow = '';
+//     };
+//   }, [isOpen, onClose]);
+
+//   if (!isOpen) return null;
+//   if (typeof document === 'undefined') return null;
+
+//   return createPortal(
+//     <div
+//       className={css.backdrop}
+//       role="dialog"
+//       aria-modal="true"
+//       onClick={(e) => {
+//         if (e.target === e.currentTarget) {
+//           onClose();
+//         }
+//       }}
+//     >
+//       <div className={css.modal}>{children}</div>
+//     </div>,
+//     document.body
+//   );
+// }
+
 'use client';
 
 import { useEffect } from 'react';
@@ -6,19 +62,12 @@ import type { ReactNode } from 'react';
 import css from './Modal.module.css';
 
 interface ModalProps {
-  isOpen: boolean;
   onClose: () => void;
   children: ReactNode;
 }
 
-export default function Modal({
-  isOpen,
-  onClose,
-  children,
-}: ModalProps) {
+export default function Modal({ onClose, children }: ModalProps) {
   useEffect(() => {
-    if (!isOpen) return;
-
     const handleEsc = (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
         onClose();
@@ -32,9 +81,8 @@ export default function Modal({
       document.removeEventListener('keydown', handleEsc);
       document.body.style.overflow = '';
     };
-  }, [isOpen, onClose]);
+  }, [onClose]);
 
-  if (!isOpen) return null;
   if (typeof document === 'undefined') return null;
 
   return createPortal(
@@ -53,4 +101,3 @@ export default function Modal({
     document.body
   );
 }
-
