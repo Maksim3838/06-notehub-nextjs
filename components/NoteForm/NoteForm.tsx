0a3c3c46b6d4166dada6956519cc8e1a31bb7f92
Maksim <70702008m@gmail.com>
2025-12-28 20:12:22 +0200

    text

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,7 +1,7 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createNote } from "../../services/noteService";
+import { createNote } from "../../lib/api";
 import type { Note } from "../../types/note";
 import css from "./NoteForm.module.css"
 
